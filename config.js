fba40a2fd4f9c1a5048b7aedbf0a2aae13741a67
Nadiyamax2234 <134248594+Nadiyamax2234@users.noreply.github.com>
2023-05-30 21:57:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94750208136', 'sandaru', true],
+  ['947195321658', 'Mr nadiya ', true], 
   [''] 
 ] //Number of owners
 
